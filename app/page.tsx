64b540da43a515e5a897af6549b7317689ee008e
yonasketema <yonask.kitaw@gmail.com>
2024-01-29 21:10:26 +0300

    list book from db

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-  return <main>Next-BookStore</main>;
+import BookList from "@/components/BookList";
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+async function getAllBook() {
+  const books = await prisma.book.findMany({});
+  return books;
 }
+
+async function Home() {
+  const books = await getAllBook();
+
+  return <BookList books={books} />;
+
+  {
+    /* new-book best-book-list author-list */
+  }
+}
+
+export default Home;
