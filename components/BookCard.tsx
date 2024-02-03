21c9b11c8068bca76d8fa08a4c17fb919a89deb3
yonasketema <yonask.kitaw@gmail.com>
2024-02-03 20:46:34 +0300

    style : add img height

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -4,10 +4,13 @@ function BookCard({ book }) {
   return (
     <Link href={`book/${book.id}`}>
       <li className="max-w-max space-y-2 bg-slate-50 hover:shadow-lg">
-        <img src={book.img} className="sm:h-48 " alt="" />
+        <img src={book.img} className="md:h-72" alt="" />
 
-        <div className="flex justify-between p-3">
-          <p className="font-light text-slate-800">{book.title}</p>
+        <div className="flex flex-col justify-between space-y-2 p-3">
+          <p className="text-sm font-semibold text-slate-800">{book.title}</p>
+          <p className="font- font-light capitalize text-slate-500">
+            by {book.author}
+          </p>
           <span className="text-slate-600">{book.rating} ⭐️</span>
         </div>
       </li>
