adc4885ed0bdfbc0b7da2d790385ed26e2d052cc
yonasketema <yonask.kitaw@gmail.com>
2024-02-03 19:39:23 +0300

    fix:image path

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 function BookCard({ book }) {
   return (
     <Link href={`book/${book.id}`}>
-      <li className="bg-slate-50 max-w-max space-y-2">
-        <img src={"yon.jpg"} className="sm:h-48 " alt="" />
+      <li className="max-w-max space-y-2 bg-slate-50 hover:shadow-lg">
+        <img src={book.img} className="sm:h-48 " alt="" />
 
         <div className="flex justify-between p-3">
-          <p className="text-slate-800 font-light">{book.title}</p>
+          <p className="font-light text-slate-800">{book.title}</p>
           <span className="text-slate-600">{book.rating} ⭐️</span>
         </div>
       </li>
